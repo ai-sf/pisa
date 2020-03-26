730c99bfa0e2ecc15942b16087ad81957a3cc858
Damiano Lucarelli <lucarelli.damiano.ld@gmail.com>
2020-03-26 11:36:57 +0100

    test_fix_v-scroll

--- js/slider.js
+++ js/slider.js
@@ -230,7 +230,7 @@
             panning = false;
             curr_index = $slider.find('.active').index();
 
-            if (!swipeRight && !swipeLeft || $slides.length <=1) {
+            if (!swipeRight && !swipeLeft && !scrolling || $slides.length <=1) {
               // Return to original spot
               $curr_slide.velocity({ translateX: 0
                   }, {duration: 300, queue: false, easing: 'easeOutQuad'});
